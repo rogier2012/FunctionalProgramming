2d6ea93ad92dd88dd4205dab06226a20485e86ad
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-05-24 12:31:28 +0200

    ex1

--- src/Block4/Exercise4_2.hs
+++ src/Block4/Exercise4_2.hs
@@ -1,13 +1,16 @@
 module Block4.Exercise4_2 where
+import Data.Maybe
 
 data Expr   = Const Int
             | Var String
             | BinOp String Expr Expr
             | App Expr Expr
+--            | If Expr String Expr Then Expr Else Expr
 
 data Type   = IntType
+            | BoolType
             | FunType Type Type
-            deriving Show
+            deriving (Show,Eq)
 
 type Env = [(String, Type)]
 
@@ -17,23 +20,28 @@ environment = [("+", FunType IntType (FunType IntType IntType)), ("-",  FunType 
 typeOf :: Env -> Expr -> Type
 typeOf env (Const a) = IntType
 typeOf env (Var a)
-        | t == Nothing                  = error ”...”
+        | t == Nothing                  = error "..."
         | otherwise                     = fromJust t
-         where
-         t = lookup x env
+            where
+                t = lookup a env
 typeOf env (BinOp op e e') = case top of
-        Nothing                         -> error ""
+        Nothing                         -> error "..."
 
         Just (FunType t0 (FunType t1 t2))
             | t0 == te && t1 == te'     -> t2
-            | otherwise                 -> error ""
+            | otherwise                 -> error "..."
 
-        Just _                          -> error ""
-       where
-           top  = lookup op env
-           te   = typeOf env e
-           te'  = typeOf env e'
+        Just _                          -> error "..."
+    where
+       top  = lookup op env
+       te   = typeOf env e
+       te'  = typeOf env e'
 
-
+typeOf env (App f x) = typeOf env f
+--        | tf == (FunType tx t1)         = t1
+--        | otherwise                     = error ""
+--            where
+--                tf = typeOf env f
+--                tx = typeOf env x
 
 
